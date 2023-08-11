ed6fc221028317da0802e6a0a29442eaa43d6c75
skillfuldev <chernonog.evgeniy@gmail.com>
2023-08-11 13:02:44 +0300

    add_fix_H2_console

--- working/src/main/java/ua/chernonog/working/antifraud/security/SecurityConfig.java
+++ working/src/main/java/ua/chernonog/working/antifraud/security/SecurityConfig.java
@@ -5,27 +5,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 //@EnableWebSecurity
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                //Веключаем способ передачи логина/пароля
-                .httpBasic(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
+
+        //Веключаем способ передачи логина/пароля
+//                .httpBasic(Customizer.withDefaults())
+//
+//                .headers(f->f.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+//                .csrf(d->d.ignoringRequestMatchers("/h2/**").disable())
+//
+////
 //                .headers(c -> c.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .authorizeHttpRequests(c -> c.requestMatchers(
-                                "/api/auth/user").permitAll().anyRequest().permitAll())
-//                        .requestMatchers( "/actuator/shutdown").permitAll())
+//               .securityMatcher("/h2/**")
+//                .authorizeHttpRequests(c -> c.requestMatchers(
+//                                "/api/auth/user").permitAll().anyRequest().permitAll())
+//                .sessionManagement(session -> session
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//
+//                .build();
+        http.authorizeHttpRequests(c -> c.requestMatchers(toH2Console())
+                        .permitAll())
+                .csrf(c -> c.ignoringRequestMatchers(toH2Console()));
+
+
+        return http.authorizeHttpRequests(c -> c.requestMatchers(antMatcher(
+                                "/api/auth/user")).permitAll()
+                        .anyRequest().permitAll())
                 .build();
 
     }
--- working/src/main/java/ua/chernonog/working/antifraud/service/UserService.java
+++ working/src/main/java/ua/chernonog/working/antifraud/service/UserService.java
@@ -1,22 +1,30 @@
 package ua.chernonog.working.antifraud.service;
 
+
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ua.chernonog.working.antifraud.entity.UserEntity;
 import ua.chernonog.working.antifraud.mapper.UserResToEntity;
 import ua.chernonog.working.antifraud.model.request.UserReq;
 import ua.chernonog.working.antifraud.model.respons.UserRes;
 import ua.chernonog.working.antifraud.repository.UserRepository;
 
+@Slf4j
 @AllArgsConstructor
 @Service
+@Transactional
 public class UserService {
     UserRepository userRepository;
 
     public void saveUser(UserReq user) {
-     UserEntity userEntity= new UserEntity();
-     userEntity.builder().name(user.getName()).username(user.getUsername()).password(user.getPassword()).build();
-
-        userRepository.save(userEntity);
+        log.info("name={} username={} password={}",
+                user.getName(), user.getUsername(), user.getPassword());
+        userRepository.save(UserEntity.builder()
+                .name(user.getName())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .build());
     }
 }
