fb0a8ed18ce911767c24214d7ed2c2e5f96257a7
skillfuldev <chernonog.evgeniy@gmail.com>
2023-08-24 10:48:49 +0300

    add_minorChange

--- working/src/main/java/ua/chernonog/working/antifraud/controller/UserController.java
+++ working/src/main/java/ua/chernonog/working/antifraud/controller/UserController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.chernonog.working.antifraud.model.request.UserReq;
 import ua.chernonog.working.antifraud.model.respons.UserDelRes;
@@ -25,8 +24,6 @@ public class UserController {
     public UserRes regUser(@Valid @RequestBody UserReq user) {
         log.info("request = {}", user);
         return userService.saveUser(user);
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(new UserRes(2222,"SAM","BEAN"));
     }
 
     @GetMapping("/api/auth/list")
