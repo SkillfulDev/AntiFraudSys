22a363929619dced96b64b3d091440f713b81a66
skillfuldev <chernonog.evgeniy@gmail.com>
2023-09-27 10:14:07 +0300

    add_Transaction_change

--- working/src/main/java/ua/chernonog/working/antifraud/service/TransactionService.java
+++ working/src/main/java/ua/chernonog/working/antifraud/service/TransactionService.java
@@ -11,36 +11,59 @@ import ua.chernonog.working.antifraud.repository.IpRepository;
 import ua.chernonog.working.antifraud.repository.StolenCardRepository;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class TransactionService {
     IpRepository ipRepository;
     StolenCardRepository stolenCardRepository;
-    public TransactionResponse getResultOfPermission(long amount,String ip,String number) {
+
+    public TransactionResponse getResultOfPermission(long amount, String ip, String number) {
 
         TransactionResponse response = new TransactionResponse();
-        boolean isValidNumber =stolenCardRepository.existsByNumberIgnoreCase(number);
+        boolean isValidNumber = stolenCardRepository.existsByNumberIgnoreCase(number);
         boolean isValidIp = ipRepository.existsByIp(ip);
 
-        if (amount<0) {
+
+        if (amount < 0) {
             throw new ErrorResponseException(HttpStatus.BAD_REQUEST);
-        } else if (amount <= 1500&&amount>200
-                &&!isValidIp&&!isValidNumber) {
+        } else if (amount <= 1500 && amount > 200
+                && !isValidIp && !isValidNumber) {
             response.setResult(Permission.MANUAL_PROCESSING);
-            if (isValidIp) {
-                response.setInfo(Reason.IP + "," + Reason.AMOUNT);
-            }
+            getReason(amount, isValidNumber, isValidIp);
+
         } else if (amount <= 200
-                &&!isValidIp&&!isValidNumber) {
+                && !isValidIp && !isValidNumber) {
             response.setResult(Permission.ALLOWED);
             response.setInfo("none");
-        }else {
+
+        } else {
             response.setResult(Permission.PROHIBITED);
-//
+
         }
         return response;
     }
 
+    private List<String> getReason(long amount, boolean isValidNumber, boolean isValidIp) {
+        List<String> rejection = new ArrayList<>();
+        rejection.add(List.of("card_number", "ip").toString());
+
+
+        if (!isValidIp) {
+            rejection.remove("ip");
+        }
+        if (!isValidNumber) {
+            rejection.remove("card_number");
+        }
+        if (amount <= 1500 && amount > 200) {
+            rejection.add("amount");
+        }
+        return rejection;
+    }
 }
+
+
